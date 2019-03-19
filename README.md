94691dba5c83f1b608dec122ba2daa2438957042
Ranjan Shrestha <ranjan@sharingapples.com>
2019-03-19 10:28:28 +0545

    Setup project

---
+++ README.md
@@ -0,0 +1,28 @@
+# Usage
+```jsx
+import { createStore } from 'redux';
+import createApplication from 'redux-hooked';
+
+const { useStore, useDispatch, useAction } = createApplication(store);
+
+// Create your application specific hooks and export them
+const getConnectionStatus = state => state.status;
+export function useConnectionStatus() {
+  const { name, age } = useStore((state) => ({
+    name: state.name,
+    age: state.age,
+  }));
+
+  return useStore(getConnectionStatus);
+}
+
+const updateUrl = url => ({ type: 'UPDATE_URL', payload: url });
+export function useUpdateUrl() {
+  return useAction(updateUrl);
+}
+
+export function useUserData(id) {
+  const { name, age } = useStore(state => state.users[id], [id]);
+}
+
+```
--- src/index.js
+++ src/index.js
@@ -1,47 +1,80 @@
-import { useState, useContext } from 'react';
+import { useState, useCallback, useEffect } from 'react';
 
-function useStore(Context) {
-  return (mapState) => {
-    const store = useContext(Context);
-    const [state, setState] = useState(mapState(store.getState()));
+function shallowEqual(previous, next) {
+  if (previous === null || next === null
+    || typeof previous !== 'object' || typeof next !== 'object') {
+    return false;
+  }
+
+  if (Array.isArray(previous)) {
+    if (!Array.isArray(next)) return false;
+    if (previous.length !== next.length) return false;
+    for (let i = 0; i < previous.length; i += 1) {
+      if (next[i] !== previous[i]) return false;
+    }
+  }
+
+  const prevKeys = Object.keys(previous);
+  const nextKeys = Object.keys(next);
+  if (prevKeys.length !== nextKeys.length) return false;
+
+  for (let i = 0; i < prevKeys.length; i += 1) {
+    if (previous[prevKeys[i]] !== next[nextKeys[i]]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function storeFactory(store) {
+  return function useStore(mapState, dependencies = []) {
+    const mapper = useCallback(mapState, dependencies);
+    const [state, setState] = useState(() => mapper(store.getState()));
 
     useEffect(() => {
-      let cached = state;
+      // Keep track of the previous state to make comparison
+      let previous = state;
 
       return store.subscribe(() => {
-        const newState = mapState(store.getState());
-        if (newState === cached) {
+        const next = mapper(store.getState());
+        if (next === previous || shallowEqual(previous, next)) {
           return;
         }
 
-        cached = newState;
-        setState(newState);
+        previous = next;
+        setState(next);
       });
-    }, []);
+    }, [mapper]);
 
     return state;
-  }
+  };
 }
 
-function useDispatch(Context) {
-  return () => {
-    return useContext(Context).dispatch;
-  }
+function dispatchFactory(store) {
+  return function useDispatch() {
+    return store.dispatch;
+  };
 }
 
-function useAction(Context) {
-  return (mapActions) => {
-    const store = useContext(Context);
-    return mapActions(store.dispatch);
+function actionFactory(store) {
+  return function useAction(actionCreator) {
+    return useCallback((...args) => store.dispatch(actionCreator(...args), [actionCreator]));
+  };
+}
+
+function actionsFactory(store) {
+  return function useActions(mapActions, dependencies = []) {
+    const mapper = useCallback(mapActions, dependencies);
+    return mapper(store.dispatch);
   };
 }
 
 export default function createApplication(store) {
-  const Context = React.createContext(store);
-
   return {
-    useStore: useStore(Context),
-    useDispatch: useDispatch(Context),
-    useAction: useAction(Context),
+    useStore: storeFactory(store),
+    useDispatch: dispatchFactory(store),
+    useAction: actionFactory(store),
+    useActions: actionsFactory(store),
   };
 }
